3b94128bb292246263b1a43220964f40fee43950
權少 <ljqican@163.com>
2020-09-13 21:10:41 +0800

    添加msgpack-c庫及寫序列化庫性能測試例子

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -6,6 +6,40 @@
 #pragma warning(disable: 4127)
 #include <man.pb.h>
 #pragma warning(pop)
+#include <sstream>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <msgpack.hpp>
+
+struct Man
+{
+    int age;
+    std::string name;
+};
+
+namespace msg
+{
+    struct Man
+    {
+        int age;
+        std::string name;
+        MSGPACK_DEFINE(age, name);
+    };
+}
+
+namespace boost
+{
+    namespace serialization
+    {
+        template<class Archive>
+        void serialize(Archive& ar, Man& man, const unsigned int version)
+        {
+            (void)version;
+            ar& man.age;
+            ar& man.name;
+        }
+    }
+}
 
 static void BM_Glog(benchmark::State& state)
 {
@@ -15,7 +49,7 @@ static void BM_Glog(benchmark::State& state)
         LOG(INFO) << "";
     }
 }
-BENCHMARK(BM_Glog);
+//BENCHMARK(BM_Glog);
 
 static void BM_StringCreation(benchmark::State& state)
 {
@@ -40,19 +74,62 @@ BENCHMARK(BM_StringCopy);
 
 static void BM_Protobuf(benchmark::State& state)
 {
+    std::string str;
+    test::man man;
+    test::man man2;
     for (auto _ : state)
     {
-        test::man man;
         man.set_age(18);
         man.set_name("Tom");
 
-        std::string str;
         man.SerializeToString(&str);
 
-        test::man man2;
         man2.ParseFromString(str);
     }
 }
 BENCHMARK(BM_Protobuf);
+
+static void BM_BoostSerialization(benchmark::State& state)
+{
+    Man man;
+    Man man2;
+    std::string buffer;
+    for (auto _ : state)
+    {
+        man.age = 18;
+        man.name = "Tom";
+        std::stringstream obuffer;
+        boost::archive::text_oarchive otext(obuffer);
+        otext << man;
+        buffer = obuffer.str();
+
+        std::stringstream ibuffer(buffer);
+        boost::archive::text_iarchive itext(ibuffer);
+        itext >> man2;
+    }
+}
+BENCHMARK(BM_BoostSerialization);
+
+static void BM_Msgpack(benchmark::State& state)
+{
+    msg::Man man;
+    msg::Man man2;
+
+    for (auto _ : state)
+    {
+        man.age = 18;
+        man.name = "Tom";
+        msgpack::sbuffer sbuf;
+        msgpack::pack(sbuf, man);
+
+        msgpack::object_handle oh =
+            msgpack::unpack(sbuf.data(), sbuf.size());
+
+        msgpack::object obj = oh.get();
+
+        obj.convert(man2);
+    }
+}
+BENCHMARK(BM_Msgpack);
 
 BENCHMARK_MAIN();
